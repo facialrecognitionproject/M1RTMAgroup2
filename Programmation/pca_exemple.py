e5baab4990863b22395d635398160b2e02be354b
facialrecognitionproject <rtma.projet@gmail.com>
2015-05-29 13:37:21 +0200

    changement
    j'ai changer un fichier pour tester le git.

--- Programmation/pca_exemple.py
+++ Programmation/pca_exemple.py
@@ -164,14 +164,14 @@ subplot(title="Eigenfaces AT&T Facedatabase", images=E, rows=4, cols=4, sptitle=
 #from ScriptsModules.methods import project, reconstruct
 
 # reconstruction steps
-steps=[i for i in xrange(10, min(len(X), 320), 20)]
-E = []
-for i in xrange(min(len(steps), 16)):
-	numEvs = steps[i]
-	P = project(W[:,0:numEvs], X[0].reshape(1,-1), mu)
-	R = reconstruct(W[:,0:numEvs], P, mu)
-	# reshape and append to plots
-	R = R.reshape(X[0].shape)
-	E.append(normalize(R,0,255))
-# plot them and store the plot to "python_reconstruction.pdf"
-subplot(title="Reconstruction AT&T Facedatabase", images=E, rows=4, cols=4, sptitle="Eigenvectors", sptitles=steps, colormap=cm.gray, filename="python_pca_reconstruction.pdf")
+#steps=[i for i in xrange(10, min(len(X), 320), 20)]
+#E = []
+#for i in xrange(min(len(steps), 16)):
+#	numEvs = steps[i]
+#	P = project(W[:,0:numEvs], X[0].reshape(1,-1), mu)
+#	R = reconstruct(W[:,0:numEvs], P, mu)
+#	# reshape and append to plots
+#	R = R.reshape(X[0].shape)
+#	E.append(normalize(R,0,255))
+## plot them and store the plot to "python_reconstruction.pdf"
+#subplot(title="Reconstruction AT&T Facedatabase", images=E, rows=4, cols=4, sptitle="Eigenvectors", sptitles=steps, colormap=cm.gray, filename="python_pca_reconstruction.pdf")
